9a14e09315ced6c655a4207d13d950c61315937d
rashmi2800 <rashmishettar2804@gmail.com>
2022-02-10 09:06:46 +0530

    changes

---
+++ 3_Implementation/inc/headders.c
@@ -0,0 +1,64 @@
+/**
+ * @file test.c
+ * @author Rashmi Shidramshettar
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-10
+ * 
+ * @copyright 
+ * 
+ */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void login();
+void mainmenu();
+
+
+struct motor
+{
+	char mt_name[30];
+	char appln[70];
+	int speed;
+	float price;
+};
+
+
+void mainmenu()
+/**
+* assign i.j value to 0
+* brief structure for motor Record
+*
+*
+*/
+
+while(j!=6)
+/**
+*
+* @brief function to take input from users
+* 
+*
+*/
+
+switch (j)
+/**
+*
+*
+* @brief cases for all the operations.
+*
+*
+*/
+
+void login()
+/**
+*
+*
+* @ Function to display login screen
+ * strcmp fuction to compare the string 
+ *
+ */ 
+
+#endif 
